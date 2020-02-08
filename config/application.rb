d2be2feb19687d82ee89eb8ca36a0403ad94d60d
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-08 16:18:09 +0900

    add config:rspec

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module QuestionBox
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
