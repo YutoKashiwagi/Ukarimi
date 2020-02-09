5abc2797675ca7853dfe92757f892105ff0f10d9
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-09 22:15:21 +0900

    get user_path after sign upf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#home'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   resources :users, only: [:show]
 end
