ff86d2931ffe2ca625e57b52fd695935a0b05bd4
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-09 17:35:36 +0900

    about user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { build(:user) }
+
+  it '有効なファクトリを持つこと' do
+    expect(user.valid?).to eq true
+  end
+
+  describe 'バリデーション' do
+    context 'name' do
+      it 'presense :true' do
+        user.name = ''
+        user.valid?
+        expect(user.errors[:name]).to include("can't be blank")
+      end
+
+      it 'length: {maximum: 20}' do
+        user.name = 'a' * 21
+        user.valid?
+        expect(user.errors[:name]).to include("is too long (maximum is 20 characters)")
+      end
+    end
+  end
 end
