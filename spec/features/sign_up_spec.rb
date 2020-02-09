21b728f8964a78f30b5f831d0c8f0ee6bdd38203
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-09 20:10:02 +0900

    add rspec:feature sign up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "SignUps", type: :feature do
-  # pending "add some scenarios (or delete) #{__FILE__}"
+  it '正常にサインアップできること' do
+    visit new_user_registration_paht
+    fill_in 'Name', with: 'foobar'
+    fill_in 'Email', with: 'foobar@foobar.com'
+    fill_in 'Password', with: 'foobar'
+    fill_in 'Password confirmation', with: 'foobar'
+    click_button 'Sign up'
+    expect(page).to have_content 'foobar'
+  end
 end
