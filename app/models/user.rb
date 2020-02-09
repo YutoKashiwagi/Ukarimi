920d4fea5c97821e0f5ca22e01c401ce34176e5b
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-09 17:33:21 +0900

    validates :name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # バリデーション
+  validates :name,
+            presence: true,
+            length:   {maximum: 20}
 end
