2b3ef818325d51f5aa7ef9ce2b5555e5cdb78c45
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-10 17:51:22 +0900

    config.generators controller_specs: false

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module QuestionBox
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: true,
+        controller_specs: false,
         request_specs: false
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
