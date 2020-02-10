0215d52f77d99ab7bc2d4aaca652c663118bbebe
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-10 09:54:17 +0900

    cosmetic changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,8 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
-    def after_sign_up_path_for(resource)
-      user_path(current_user)
-    end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
   }
 
   resources :users, only: [:show]
