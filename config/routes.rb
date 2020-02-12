0bfd66b13273884011801d91965e344d40cbad35
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-13 03:47:55 +0900

    add answer routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: [:show]
-  resources :questions, only: [:index, :show, :create, :destroy]
+  resources :questions, only: [:index, :show, :create, :destroy] do
+    resources :answers, only: [:create, :destroy]
+  end
 end
