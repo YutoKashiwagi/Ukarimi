ceb95d695d14af2b176d474a52fe4347f02f6f18
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-16 20:05:20 +0900

    answerモデルテスト追加

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:answer) { build(:answer) }
+
+  it '有効なファクトリを持つこと' do
+    expect(answer.valid?).to eq true
+  end
+
+  describe 'バリデーション' do
+    context 'content' do
+      subject { answer.errors[:content] }
+
+      it 'presence: true' do
+        answer.content = ''
+        answer.valid?
+        is_expected.to include("can't be blank")
+      end
+
+      it 'length: { maximum: 1000 }' do
+        answer.content = 'a' * 1001
+        answer.valid?
+        is_expected.to include("is too long (maximum is 1000 characters)")
+      end
+    end
+  end
 end
