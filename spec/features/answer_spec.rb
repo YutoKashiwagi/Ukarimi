b3e15c6c8c024a70390ab0f21243786bd9c4db4a
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-17 13:37:40 +0900

    font-awesome導入

--- spec/features/answer_spec.rb
+++ spec/features/answer_spec.rb
@@ -53,28 +53,29 @@ RSpec.feature "Answers", type: :feature do
     let!(:answer_second) { create(:answer, content: '二番目の回答', user: user_second, question: taro_q) }
 
     it '回答者しか自分の回答を削除できないこと' do
+      # user_thirdがuser_firstの回答を削除しようとする
       login_as user_third, scope: :user
       visit question_path(taro_q.id)
-      expect(page).not_to have_content 'delete'
+      expect(page).not_to have_selector ".delete-#{answer_first.id}"
     end
 
     it '回答者は自分の投稿を削除できること' do
       login_as user_first, scope: :user
       visit question_path(taro_q.id)
       expect(page).to have_content '一番目の回答'
-      expect { click_link 'delete' }.to change { taro_q.answers.count }.by(-1)
+      expect { find(".delete-#{answer_first.id}").click }.to change { taro_q.answers.count }.by(-1)
       expect(page).not_to have_content '一番目の回答'
     end
 
     # 一つの回答に対して、同一人物が複数の回答を投稿している時
     # その人物が一つの回答を削除すると、残りも削除されてしまう
     it 'バグ再現テスト' do
-      ans_a = create(:answer, user: user_third, question: taro_q, content: '回答a')
-      create(:answer, user: user_third, question: taro_q, content: '回答b')
+      answer_third = create(:answer, user: user_third, question: taro_q, content: 'answer_thirdの回答1')
+      create(:answer, user: user_third, question: taro_q, content: 'answer_thirdの回答2')
       login_as user_third, scope: :user
       visit question_path(taro_q.id)
-      expect { find(".delete-#{ans_a.id}").click }.to change { user_third.answers.count }.by(-1)
-      expect(page).to have_content '回答b'
+      expect { find(".delete-#{answer_third.id}").click }.to change { user_third.answers.count }.by(-1)
+      expect(page).to have_content 'answer_thirdの回答2'
     end
   end
 end
