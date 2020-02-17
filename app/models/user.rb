73ed077a40088e452fe86724a2040a6597360c42
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-17 13:04:13 +0900

    dependent: : destroyの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :questions
-  has_many :answers
+  has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   # バリデーション
   validates :name,
