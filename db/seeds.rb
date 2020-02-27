3cc40ea313d6efa06ed5cac93f14f4468f6f3ab8
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-27 22:46:20 +0900

    データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
-User.create!(
+require 'faker'
+
+Faker::Config.locale = :ja
+
+hoge = User.create(
   name: "hogehoge",
   email: "hogehoge@hogehoge.com",
   password: "hogehoge"
 )
 
+tanaka = User.create(
+  name: '田中太郎',
+  email: 'tanaka@tanaka.com',
+  password: 'password'
+)
+
+question1 = Question.create(
+  user_id: tanaka.id,
+  title: '数学のオススメ参考書',
+  content: '現在高校一年生です。理系の大学に進学したいので、オススメの参考書を教えてください'
+)
+
+answer1 = question1.answers.create(
+  user_id: hoge.id,
+  content: '基礎問題精講シリーズがオススメです'
+)
+
 10.times do |n|
-  name = "sample"
+  name = Faker::Name.name
   email = "email-#{n + 1}@email.com"
   password = "password"
   User.create!(
@@ -16,11 +37,18 @@ User.create!(
 end
 
 # Question
-10.times do 
-  user = User.first
+User.ids.each do |u_id|
   Question.create!(
-    user_id: user.id,
+    user_id: u_id,
     title: "question title",
     content: "question content"
+  )
+end
+
+Question.ids.each do |q_id|
+  Answer.create(
+    user_id: User.ids.sample,
+    question_id: q_id,
+    content: '頑張ってください'
   )
 end
