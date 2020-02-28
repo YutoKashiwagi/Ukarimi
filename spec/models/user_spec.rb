1c796dca11356ba0ac35b93d03d2450567336cc2
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-28 22:51:09 +0900

    ストック機能のテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+  let(:question) { create(:question) }
 
   it '有効なファクトリを持つこと' do
     expect(user.valid?).to eq true
@@ -37,6 +38,44 @@ RSpec.describe User, type: :model do
         user.password = 'a' * 5
         user.valid?
         is_expected.to include("is too short (minimum is 6 characters)")
+      end
+    end
+  end
+
+  describe 'ストック' do
+    before { user.save }
+
+    describe 'stocked?(question)' do
+      example 'ストックしている場合、trueを返すこと' do
+        user.stocked_questions << question
+        expect(user.stocked?(question)).to eq true
+      end
+
+      example 'ストックしていない場合、falseを返すこと' do
+        expect(user.stocked?(question)).to eq false
+      end
+    end
+
+    describe 'stock(question)' do
+      example 'ストックしていない場合、ストック出来ること' do
+        user.stock(question)
+        expect(user.stocked?(question)).to eq true
+      end
+
+      example 'ストックしている場合、nilを返すこと' do
+        user.stock(question)
+        expect(user.stock(question)).to eq nil
+      end
+    end
+
+    describe 'unstock(question)' do
+      example 'ストックしている場合、ストックを解除できること' do
+        user.stock(question)
+        expect { user.unstock(question) }.to change { user.stocked_questions.count }.by(-1)
+      end
+
+      example 'ストックしていない場合、nilを返すこと' do
+        expect(user.unstock(question)).to eq nil
       end
     end
   end
