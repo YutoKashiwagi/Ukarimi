6825252c3c5b3dcfdd45a87822e13b4176552154
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-01 02:12:14 +0900

    コメントのテスト

---
+++ spec/features/comments/questions_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.feature "Comments::Questions", type: :feature do
+  include Warden::Test::Helpers
+
+  let!(:user) { create(:user) }
+  let!(:question) { create(:question) }
+  let!(:answer) { create(:answer, question: question) }
+  let!(:comment) { create(:comment, commentable: question) }
+
+  describe 'ログインしていないとき' do
+    example 'コメントフォームが存在しないこと' do
+      expect(page).not_to have_selector ".comment_form_#{comment.commentable.id}"
+    end
+  end
+
+  describe 'Create' do
+    before do
+      login_as user, scope: :user
+      visit question_path(question.id)
+    end
+    example '正常に作成できること' do
+      within('.question_container') do
+        fill_in 'comment[content]', with: 'ああああああ'
+        expect{click_button 'コメントする'}.to change{ question.comments.count }.by(1)
+      end
+      expect(page).to have_content 'あああああ'
+    end
+    
+    example '空白のまま送信した場合、失敗すること' do
+      within('.question_container') do
+        expect{ click_button 'コメントする' }.to change{ question.comments.count }.by(0)
+      end
+      expect(page).to have_content '失敗しました'
+    end
+    
+    example '1001文字以上のコメントを送信した場合、失敗する' do
+      within('.question_container') do
+        fill_in 'comment[content]', with: 'a' * 1001
+        expect{ click_button 'コメントする' }.to change{ question.comments.count }.by(0)
+      end
+      expect(page).to have_content '失敗しました'
+    end
+  end
+  
+  describe 'Destroy' do
+    let!(:comment2) { create(:comment, commentable: question, user: user) }
+    before do
+      login_as user, scope: :user
+      visit question_path(question.id)
+    end
+
+    example 'コメントした本人ならば、コメントを削除できること' do
+      expect{ find(".delete_comment_#{comment2.id}").click }.to change{ question.comments.count }.by(-1)
+    end
+
+    example 'コメントした本人でなければ、コメントを削除できないこと' do
+      expect(page).not_to have_selector ".delete_comment_#{comment.id}"
+    end
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:answer) { create(:answer) }
+  let(:question) { create(:question) }
+  let(:comment) { build(:comment, user: user, commentable: question) }
+
+  example '有効なファクトリを持つこと' do
+    expect(comment.valid?).to eq true
+    comment.commentable = answer
+    expect(comment.valid?).to eq true
+  end
+
+  describe 'バリデーション' do
+    example 'userがいなければいけないこと' do
+      comment.user = nil
+      comment.valid?
+      expect(comment.errors[:user]).to include('must exist')
+    end
+
+    example 'commentableがなければならないこと' do
+      comment.commentable = nil
+      comment.valid?
+      expect(comment.errors[:commentable]).to include('must exist')
+    end
+
+    example 'contentが空白ではいけないこと' do
+      comment.content = ''
+      comment.valid?
+      expect(comment.errors[:content]).to include("can't be blank")
+    end
+
+    describe 'contentの文字数' do
+      example '1000文字以下' do
+        comment.content = 'a' * 1000
+        expect(comment.valid?).to eq true
+      end
+
+      example '1001文字以上' do
+        comment.content = 'a' * 1001
+        comment.valid?
+        expect(comment.errors[:content]).to include('is too long (maximum is 1000 characters)')
+      end
+    end
+  end
+
+  describe 'dependent: :destroy' do
+    example 'questionに紐づいて削除されること' do
+      comment.save
+      expect{ question.destroy }.to change{ Comment.count }.by(-1)
+    end
+
+    example 'answerに紐づいて削除されること' do
+      comment.commentable = answer
+      comment.save
+      expect{ answer.destroy }.to change{ Comment.count }.by(-1)
+    end
+
+    example 'userに紐づいて削除されること' do
+      comment.save
+      expect{ user.destroy }.to change{ Comment.count }.by(-1)
+    end
+  end
 end
