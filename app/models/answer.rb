0eb1a7ac73a5367ce3488ed46bc65ffd3c34b14c
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-02-29 21:59:15 +0900

    dependent: :destroy追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,8 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
+  has_many :comments, as: :commentable, dependent: :destroy
+
   # バリデーション
   validates :content,
             presence: true,
