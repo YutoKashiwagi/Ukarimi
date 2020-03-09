06d49cbe53d9c723158daa9955d26817d0806fd1
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-10 01:22:15 +0900

    [rspec:requests] comment shared_examplesを使って共通化

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe "Comments", type: :request do
   let!(:user) { create(:user) }
   let!(:other_user) { create(:user) }
-  let!(:question) { create(:question) }
 
-  describe 'commetable => question' do
-    let!(:comment) { create(:comment, user: user, commentable: question) }
-
+  shared_examples 'comment#create, destroy' do
+    let!(:comment) { create(:comment, commentable: commentable) }
     describe 'create' do
       context 'ログインしている時' do
         before { sign_in user }
+
         example '正常に作成できること' do
           expect do
-            post question_comments_path(question.id), params: { comment: { user_id: user.id, content: 'content' } }
-          end.to change { question.comments.count }.by(1)
+            post create_comment_path, params: { comment: { user_id: user.id, content: 'content' } }
+          end.to change { commentable.comments.count }.by(1)
         end
       end
 
       context 'ログインしていない時' do
-        before { post question_comments_path(question.id), params: { comment: { user_id: user.id, content: 'content' } } }
+        before { post create_comment_path, params: { comment: { user_id: user.id, content: 'content' } } }
+
         example 'サインイン画面へリダイレクトされること' do
           expect(response).to redirect_to new_user_session_path
         end
@@ -27,18 +27,22 @@ RSpec.describe "Comments", type: :request do
     end
 
     describe 'destroy' do
+      let!(:comment) { create(:comment, user: user, commentable: commentable) }
+
       context 'ログインしている時' do
         context '本人の場合' do
           before { sign_in user }
+
           example '正常に削除できること' do
             expect do
               delete comment_path(comment.id)
-            end.to change { question.comments.count }.by(-1)
+            end.to change { commentable.comments.count }.by(-1)
           end
         end
 
         context '本人でない場合' do
           before { sign_in other_user }
+
           example 'エラーが発生すること' do
             expect do
               delete comment_path(comment.id)
@@ -49,10 +53,32 @@ RSpec.describe "Comments", type: :request do
 
       context 'ログインしていない時' do
         before { delete comment_path(comment.id) }
+
         example 'サインイン画面へリダイレクトされること' do
           expect(response).to redirect_to new_user_session_path
         end
       end
+    end
+  end
+
+  describe 'commentable => question' do
+    include_examples 'comment#create, destroy' do
+      let(:commentable) { create(:question, user: user) }
+      let(:create_comment_path) { question_comments_path(commentable.id) }
+    end
+  end
+
+  describe 'commentable => answer' do
+    include_examples 'comment#create, destroy' do
+      let(:commentable) { create(:answer, user: user) }
+      let(:create_comment_path) { answer_comments_path(commentable.id) }
+    end
+  end
+
+  describe 'commentable => post' do
+    include_examples 'comment#create, destroy' do
+      let(:commentable) { create(:post, user: user) }
+      let(:create_comment_path) { post_comments_path(commentable.id) }
     end
   end
 end
