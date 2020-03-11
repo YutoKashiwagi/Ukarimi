e7a00bb453c0817bb820e0042c7d2e777a2cceed
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-12 00:23:35 +0900

    [rspec][fix] i18nによる日本語化に伴う修正

--- spec/system/sign_ups_spec.rb
+++ spec/system/sign_ups_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe "SignUps", type: :system do
   example '正常にサインアップできること' do
     visit new_user_registration_path
     fill_in 'Name', with: 'foobar'
-    fill_in 'Email', with: 'foobar@foobar.com'
-    fill_in 'Password', with: 'foobar'
-    fill_in 'Password confirmation', with: 'foobar'
-    click_button 'Sign up'
+    fill_in 'メールアドレス', with: 'foobar@foobar.com'
+    fill_in 'パスワード', with: 'foobar'
+    fill_in '確認用パスワード', with: 'foobar'
+    click_button 'アカウント登録'
     expect(page).to have_content 'foobar'
   end
 end
