4fa8e6ff1a02162b5d8611edbd71f402a0d3d93f
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-12 21:26:35 +0900

    [model][user]回答した質問を取得する処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :answered_questions, through: :answers, source: :question
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
