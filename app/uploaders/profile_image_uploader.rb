6bc36a32737821beb5db18dd167210eb0330ab3b
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-13 18:20:15 +0900

    rubocop

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -15,10 +15,10 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     'default.jpg'
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
   # Process files as they are uploaded:
@@ -36,7 +36,7 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png )
+    %w(jpg jpeg gif png)
   end
 
   # Override the filename of the uploaded files:
