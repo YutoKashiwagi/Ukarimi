ee0a3ac297d0528e07309219be6c863933446c59
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-13 17:56:29 +0900

    カラム追加、マイグレーション実行直後

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200305174535) do
+ActiveRecord::Schema.define(version: 20200312131332) do
 
   create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text "content"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20200305174535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
