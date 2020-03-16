edc1f4e5fcc1915a9f966e887b9e0ce71440bdcb
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-16 15:05:08 +0900

    [models][question]decide_best_answer()メソッドの 定義

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,13 +6,18 @@ class Question < ApplicationRecord
   belongs_to :user
 
   has_many :answers, dependent: :destroy
-  has_many :stocks, dependent: :destroy
-  has_many :stocked_users, through: :stocks, source: :user
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likable, dependent: :destroy
+
+  # ストック周り
+  has_many :stocks, dependent: :destroy
+  has_many :stocked_users, through: :stocks, source: :user
+
+  # カテゴリー周り
   has_many :tag_relationships, as: :taggable, dependent: :destroy
   has_many :categories, through: :tag_relationships, source: :category
 
+  # ベストアンサー周り
   has_one :q_and_a_relationship
   has_one :best_answer, through: :q_and_a_relationship, source: :answer
 
@@ -26,5 +31,11 @@ class Question < ApplicationRecord
 
   def has_best_answer?
     best_answer.present?
+  end
+
+  def decide_best_answer(answer)
+    if answers.include?(answer) && best_answer.blank?
+      self.best_answer = answer
+    end
   end
 end
