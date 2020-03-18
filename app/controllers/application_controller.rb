5872708b062788d968cc5a3d83812520040cd928
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-18 18:21:48 +0900

    検索機能をヘッダーにフォームを設置する形から検索用ページで行う形に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_search
 
   def configure_permitted_parameters
     # 新規登録時
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     # 編集時
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image])
-  end
-
-  def set_search
-    @search_questions = Question.ransack(params[:q])
-    @searched_questions = @search_questions.result(distinct: true).recent
   end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,14 @@
 class SearchesController < ApplicationController
+  before_action :set_search
+
   def index
+    @categories = Category.all
+  end
+
+  private
+
+  def set_search
+    @search_questions = Question.ransack(params[:q])
+    @searched_questions = @search_questions.result(distinct: true).recent.page(params[:page]).per(10)
   end
 end
