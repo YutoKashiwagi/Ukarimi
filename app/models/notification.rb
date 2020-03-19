689cbab207ebb9d39f0b6dd1a2c3c388a4670c85
YutoKashiwagi <58618766+YutoKashiwagi@users.noreply.github.com>
2020-03-19 12:25:55 +0900

    [models][notification]通知の関連付け

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,6 @@
 class Notification < ApplicationRecord
+  belongs_to :answer, optional: true
+  belongs_to :comment, optional: true
+  belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
+  belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
 end
