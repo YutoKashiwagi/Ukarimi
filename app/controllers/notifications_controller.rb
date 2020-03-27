3eaebeb4e0bf1bcb66b00d931ae65b46c22afe08
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-03-27 20:41:25 +0900

    既読通知を残し、新着通知は色を変えて表示する仕様に変更

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,17 @@
 class NotificationsController < ApplicationController
   before_action :authenticate_user!
+  after_action :check_notifications
 
   def index
-    @notifications = current_user.passive_notifications.where(checked: false).
+    @notifications = current_user.passive_notifications.
       includes(:visitor, :visited).page(params[:page]).per(10)
-    @notifications.each do |notification|
+  end
+
+  private
+
+  def check_notifications
+    notifications = current_user.passive_notifications.includes(:visitor, :visited)
+    notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
   end
