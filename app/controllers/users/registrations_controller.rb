396a74233bd007abab5dfd072d262aa6d96e1606
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-03-31 21:52:16 +0900

    ゲストユーザーはユーザー情報の編集、退会ができないように設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only: [:destroy, :update]
+
   protected
 
   def after_sign_up_path_for(resource)
