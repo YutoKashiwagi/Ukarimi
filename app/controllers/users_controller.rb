049e1c5351052c7f9726d4a55f3fad31921c2642
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-04-02 16:59:34 +0900

    [user] 回答した質問にdistinct追加、pagination追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.includes(:tag_relationships, :categories, :likes).recent
-    @questions = @user.questions.includes(:best_answer, :tag_relationships, :categories).recent
-    @answered_questions = @user.answered_questions.all_includes.recent
+    @posts = @user.posts.includes(:tag_relationships, :categories, :likes).recent.page(params[:page]).per(10)
+    @questions = @user.questions.includes(:best_answer, :tag_relationships, :categories).
+      recent.page(params[:page]).per(10)
+    @answered_questions = @user.answered_questions.distinct.all_includes.recent.page(params[:page]).per(10)
   end
 end
