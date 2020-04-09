87623b79666a4a92fd7215a6546ff0b5698d27c5
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-04-09 12:45:52 +0900

    フレンドリーフォワーディング実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,4 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: [:destroy, :update]
-
-  protected
-
-  def after_sign_up_path_for(resource)
-    user_path(current_user)
-  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました'
+    redirect_to stored_location_for(:user) || root_path, notice: 'ゲストユーザーとしてログインしました'
   end
 
   # GET /resource/sign_in
