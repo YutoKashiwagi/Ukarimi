144b087afb063588f3ca8f2a7f75d88b719748ad
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-04-18 15:33:46 +0900

    通知にdependent: :destroy追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
+  has_many :notifications, dependent: :destroy
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 1000 }
 end
